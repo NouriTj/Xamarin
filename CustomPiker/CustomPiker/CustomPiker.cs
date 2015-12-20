a11ebfe30918c3cb0aafd1dd0049eb4ff264d23d
NouriTj <user@MacBook.local>
2015-12-20 13:26:10 +0100

    Adding CustomPiker sample
    Adding example custom piker on Android with Xamarin.forms

---
+++ CustomPiker/CustomPiker/CustomPiker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CustomPiker
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			Picker pickerArrival = new Picker ();
+			pickerArrival.Items.Add ("Android");
+			pickerArrival.Items.Add ("iOS");
+			pickerArrival.Items.Add ("Windows Phone");
+
+			Button bt = new Button {
+				Text = "Select OS",
+			};
+
+			bt.Clicked += (e, sender) => {
+				Console.WriteLine ("Selected item: " + pickerArrival.Items [pickerArrival.SelectedIndex] +
+				" - index: " + pickerArrival.SelectedIndex);
+			};
+
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						pickerArrival,
+						bt
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ CustomPiker/Droid/Renderers/CustomPickerRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Android.Widget;
+using System.Collections.Generic;
+using Xamarin.Forms.Platform.Android;
+
+using CustomPiker.Droid;
+
+[assembly: ExportRenderer (typeof(Picker), typeof(CustomPickerRenderer))]
+namespace CustomPiker.Droid
+{
+	public class CustomPickerRenderer : ViewRenderer<Picker,Spinner>
+	{
+		Picker picker;
+
+		protected override void OnElementChanged (ElementChangedEventArgs<Picker> e)
+		{
+			base.OnElementChanged (e);
+			if (e.OldElement != null || Element == null) {
+				return;
+			}
+			picker = e.NewElement;
+			IList<string> scaleNames = e.NewElement.Items;
+			Spinner spinner = new Spinner (this.Context);
+
+			spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs> (spinner_ItemSelected);
+
+			var scaleAdapter = new  ArrayAdapter<string> (this.Context, Android.Resource.Layout.SimpleSpinnerItem, scaleNames);
+
+			scaleAdapter.SetDropDownViewResource (Android.Resource.Layout.SimpleSpinnerDropDownItem);
+
+			spinner.Adapter = scaleAdapter;
+
+			base.SetNativeControl (spinner);
+		}
+
+		private void spinner_ItemSelected (object sender, AdapterView.ItemSelectedEventArgs e)
+		{
+			picker.SelectedIndex = (e.Position);
+		}
+	}
+}
